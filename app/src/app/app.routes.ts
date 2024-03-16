fc4f079a521bff8fbe7206d45e53373eed267c34
trueno <80685257+truenostyle@users.noreply.github.com>
2024-03-16 13:40:26 +0200

    main page + fix bugs

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { RegisterComponent } from './components/register/register.component';
 
+
 export const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: 'main', component: MainComponent },
---
+++ app/src/app/models/list-item.model.ts
@@ -0,0 +1,9 @@
+export interface ListItem {
+    imageUrl: string;
+    name: string;
+    starImageUrl: string;
+    rating: number;
+    place: string;
+    time: string;
+    price: number;
+  }
