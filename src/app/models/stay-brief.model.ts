dfdb9185c0eff0a8f92db138ce1ad35b4ed58a97
trueno <80685257+truenostyle@users.noreply.github.com>
2024-03-24 08:01:27 +0200

    backend main page

---
+++ src/app/models/stay-brief.model.ts
@@ -0,0 +1,10 @@
+export interface StayBrief {
+    id: number;
+    name: string;
+    imageUrl: string;
+    place: string;
+    startDate: Date;
+    endDate: Date;
+    rating: number;
+    price: number;
+  }
