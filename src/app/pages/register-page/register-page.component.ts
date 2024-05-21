d888954b97c7bef84cedddbe90411c589fad5ad4
trueno <80685257+truenostyle@users.noreply.github.com>
2024-05-21 09:49:00 +0300

    final-mainpage-adaptive

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { SocialAuthService } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthResponse } from 'src/app/models/auth-response.model';
 
 @Component({
@@ -18,8 +19,37 @@ export class RegisterPageComponent {
     dateOfBirth: "2024-03-03T14:43:17.350Z",
     shareRegistrationData: false
   }
+  registerForm: FormGroup;
 
-  constructor(private socialAuthService: SocialAuthService, private http: HttpClient) { }
+  constructor(private socialAuthService: SocialAuthService, private http: HttpClient, private fb: FormBuilder) { 
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      repeatPassword: ['', [Validators.required]],
+      acceptTerms: [false, Validators.requiredTrue]
+    }, { validator: this.passwordMatchValidator });
+  }
+
+
+
+
+  passwordMatchValidator(form: FormGroup) {
+    return form.controls['password'].value === form.controls['repeatPassword'].value
+      ? null : { mismatch: true };
+  }
+
+  get name() { return this.registerForm.get('name'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get repeatPassword() { return this.registerForm.get('repeatPassword'); }
+  get acceptTerms() { return this.registerForm.get('acceptTerms'); }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // обработка формы
+    }
+  }
 
   ngOnInit() {
     this.socialAuthService.authState.subscribe((user) => {
@@ -32,15 +62,20 @@ export class RegisterPageComponent {
   }
 
   register() {
-    this.http.post<AuthResponse>(`http://localhost:5098/api/auth/register`, {
-      name: this.registerInfo.name,
-      email: this.registerInfo.email,
-      password: this.registerInfo.password,
-      gender: this.registerInfo.gender,
-      dateOfBirth: this.registerInfo.dateOfBirth,
-      shareRegistrationData: this.registerInfo.shareRegistrationData
-    })
-      .subscribe((token) => console.log(token));
-
+    if (this.registerForm.valid) {
+      this.http.post<AuthResponse>(`http://localhost:5098/api/auth/register`, {
+        name: this.registerInfo.name,
+        email: this.registerInfo.email,
+        password: this.registerInfo.password,
+        gender: this.registerInfo.gender,
+        dateOfBirth: this.registerInfo.dateOfBirth,
+        shareRegistrationData: this.registerInfo.shareRegistrationData
+      })
+        .subscribe((token) => console.log(token));
+  
+    }
+    
   }
+  
+  
 }
--- src/app/pages/register-page/register-page.module.ts
+++ src/app/pages/register-page/register-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './register-page.component';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 import { RegisterPageRoutingModule } from './register-page-routing.module';
+
 
 
 
@@ -15,10 +16,13 @@ import { RegisterPageRoutingModule } from './register-page-routing.module';
     CommonModule,
     RegisterPageRoutingModule,
     FormsModule,
-    GoogleSigninButtonModule
+    GoogleSigninButtonModule,
+    ReactiveFormsModule
   ],
   exports: [
     RegisterPageComponent
   ]
 })
-export class RegisterPageModule { }
+export class RegisterPageModule { 
+  
+}
